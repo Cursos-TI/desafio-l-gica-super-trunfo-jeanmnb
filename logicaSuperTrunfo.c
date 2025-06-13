dc04e97d53b8cc1dfeedad2d9b736123fdaa13f4
jeanmnb <jeanmarceljmd@gmail.com>
2025-06-13 14:25:06 +0000

    densidade e pibpercapita

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,89 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+
+    // Variáveis para armazenar os dados da primeira carta
+    char estado1;
+    char codigo1[4];
+    char nome_estado1[50];
+    int populacao1;
+    float area1, pib1;
+    int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;    
+
+    // Variáveis para armazenar os dados da segunda carta
+    char estado2;
+    char codigo2[4];
+    char nome_estado2[50];
+    int populacao2;
+    float area2, pib2;
+    int pontosTuristicos2;
+    float densidade2;
+    float pibPerCapita2;
+
+    // Leitura dos dados da primeira carta
+    printf("Digite as informações da Carta 1:\n");
+    printf("Estado (letra de A a H): ");
+    scanf(" %c", &estado1);
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", codigo1);
+    printf("Nome da Cidade: ");
+    scanf("%c", nome_estado1); // Lê strings com espaços
+    printf("População: ");
+    scanf("%d", &populacao1);
+    printf("Área (em km²): ");
+    scanf("%f", &area1);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib1);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos1);
+
+    // Cálculos automáticos para carta 1
+    densidade1 = populacao1 / area1;
+    pibPerCapita1 = pib1 / populacao1;
+
+
+    // Leitura dos dados da segunda carta
+    printf("\nDigite as informações da Carta 2:\n");
+    printf("Estado (letra de A a H): ");
+    scanf(" %c", &estado2);
+    printf("Código da Carta (ex: B02): ");
+    scanf("%s", codigo2);
+    printf("Nome da Cidade: ");
+    scanf(" %c", nome_estado2); // Lê strings com espaços
+    printf("População: ");
+    scanf("%d", &populacao2);
+    printf("Área (em km²): ");
+    scanf("%f", &area2);
+    printf("PIB (em bilhões de reais): ");
+    scanf("%f", &pib2);
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    // Cálculos automáticos para carta 1
+    densidade2 = populacao1 / area1;
+    pibPerCapita2 = pib2 / populacao2;
+
+    // Exibição dos dados da primeira carta
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nome_estado1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+    // Exibição dos dados da segunda carta
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nome_estado2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
